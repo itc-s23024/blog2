c25f4b50029abacc967603e71d4f569181ad2ded
又吉陸宇 <s23024@std.it-college.ac.jp>
2024-01-23 12:17:01 +0000

    2.9 1/23(火) ヒーローの画像をpropsでオンにできた。

--- components/hero.js
+++ components/hero.js
@@ -1,8 +1,9 @@
-const Hero = ({ title, subtitle }) => {
+const Hero = ({ title, subtitle, imageOn = false }) => {
   return (
     <div>
       <h1>{title}</h1>
       {subtitle}
+      {imageOn && <figure> [画像] </figure>}
     </div>
   )
 }
