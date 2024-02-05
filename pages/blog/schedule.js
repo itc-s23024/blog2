7a1e0f569182dddc8245cd3d1ef9a9ff8aefa41d
又吉陸宇 <s23024@std.it-college.ac.jp>
2024-02-05 12:29:03 +0000

    9.1 2/5(月) p248~251 アイキャッチ画像eyecatchの代替画像とブラー画像の用意ができた。 プレースホルダーのバージョンが違ったから新しくファイルを作ったりscheduleに色々追加した

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -12,6 +12,13 @@ import {
 import ConvertBody from 'components/convert-body'
 import PostCategories from 'components/post-categories'
 import Image from 'next/image'
+import { getPlaiceholder } from 'plaiceholder'
+
+// ローカルの代替アイキャッチ画像
+import { eyecatchLocal } from 'lib/constants'
+
+// Plaiceholder3用のBuffer化してから使用するように変更
+import { getImageBuffer } from 'lib/getImageBuffer'
 
 const Schedule = ({
   title,
@@ -43,6 +50,8 @@ const Schedule = ({
             height={eyecatch.height}
             sizes='(min-width: 1152px) 1152px, 100vw'
             priority
+            placeholder='blur'
+            blurDataURL={eyecatch.blurDataURL}
           />
         </figure>
 
@@ -62,18 +71,24 @@ const Schedule = ({
 }
 
 const getStaticProps = async () => {
-  const slug = 'schedule'
+  const slug = 'micro'
 
   const post = await getPostBySlug(slug)
 
   const description = extractText(post.content)
+
+  const eyecatch = post.eyecatch ?? eyecatchLocal
+
+  const imageBuffer = await getImageBuffer(eyecatch.url)
+  const { base64 } = await getPlaiceholder(imageBuffer)
+  eyecatch.blurDataURL = base64
 
   return {
     props: {
       title: post.title,
       publish: post.publishDate,
       content: post.content,
-      eyecatch: post.eyecatch,
+      eyecatch,
       categories: post.categories,
       description
     }
