599651f573dde9876169352d8d0f76dc7aab65fc
又吉陸宇 <s23024@std.it-college.ac.jp>
2024-02-05 14:45:39 +0000

    9.6 2/5(月) p272~275 記事一覧にnext/imageでアイキャッチ画像を追加できた。  Do not access Object.prototype method 'hasOwnProperty' from target object. (no-prototype-builtins) のエラーコードを治す。

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -3,12 +3,21 @@ import Meta from 'components/meta'
 import Container from 'components/container'
 import Hero from 'components/hero'
 import Posts from 'components/posts'
+import { getPlaiceholder } from 'plaiceholder'
+
+// ローカルの代替アイキャッチ画像
+import { eyecatchLocal } from 'lib/constants'
+
+// Plaiceholder3 用に書き直し
+import { getImageBuffer } from 'lib/getImageBuffer'
+
+const props = { title: 'Blog', subtitle: 'Recent Posts' }
 
 const Blog = ({ posts }) => {
   return (
     <Container>
       <Meta pageTitle='ブログ' pageDesc='ブログの記事一覧' />
-      <Hero title='Blog' subtitle='Recent Posts' />
+      <Hero {...props} />
 
       <Posts posts={posts} />
     </Container>
@@ -18,12 +27,20 @@ const Blog = ({ posts }) => {
 const getStaticProps = async () => {
   const posts = await getAllPosts()
 
+  for (const post of posts) {
+    if (!post.hasOwnProperty('eyecatch')) {
+      post.eyecatch = eyecatchLocal
+    }
+
+    const imageBuffer = await getImageBuffer(post.eyecatch.url)
+    const { base64 } = await getPlaiceholder(imageBuffer)
+    post.eyecatch.blurDataURL = base64
+  }
   return {
     props: {
       posts
     }
   }
 }
-
 export default Blog
 export { getStaticProps }
