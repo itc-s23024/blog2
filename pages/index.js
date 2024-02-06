2a46391f93b089672a2e13c4d751259f26258246
又吉陸宇 <s23024@std.it-college.ac.jp>
2024-02-06 02:02:35 +0000

    9.7 2/6(火) p276~277 トップページに記事一覧を表示できた。

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,47 @@
+import { getAllPosts } from 'lib/api'
 import Meta from 'components/meta'
 import Container from 'components/container'
 import Hero from 'components/hero'
+import Posts from 'components/posts'
+import Pagination from 'components/pagination'
+import { getPlaiceholder } from 'plaiceholder'
 
-const Home = () => {
+// ローカルの代替アイキャッチ画像
+import { eyecatchLocal } from 'lib/constants'
+
+// plaiceholder3 対応用
+import { getImageBuffer } from 'lib/getImageBuffer'
+
+const Home = ({ posts }) => {
   return (
     <Container>
       <Meta />
       <Hero title='CUBE' subtitle='アウトプットしていくサイト' imageOn />
+
+      <Posts posts={posts} />
+      <Pagination nexUrl='/blog' nextText='More Posts' />
     </Container>
   )
 }
 
+const getStaticProps = async () => {
+  const posts = await getAllPosts(4)
+
+  for (const post of posts) {
+    if (!post.hasOwnProperty('eyecatch')) {
+      post.eyecatch = eyecatchLocal
+    }
+    const imageBuffer = await getImageBuffer(post.eyecatch.url)
+    const { base64 } = await getPlaiceholder(imageBuffer)
+    post.eyecatch.blurDataURL = base64
+  }
+
+  return {
+    props: {
+      posts
+    }
+  }
+}
+
 export default Home
+export { getStaticProps }
